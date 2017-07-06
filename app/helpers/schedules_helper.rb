01269d609d22d5421724816a355a2ea48f5e46b9
emreiser <emilie.reiser@gmail.com>
2017-07-06 12:41:54 -0400

    List 5 week assignment as every week

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,7 +1,7 @@
 module SchedulesHelper
 
   def display_week_assignment(week_assignment)
-    if week_assignment.blank?
+    if week_assignment.blank? || week_assignment.length == 5
       "Every week"
     else
       week_assignment.map { |a| ScheduleVolunteer::WEEK_OPTIONS[a] }.join(", ")
