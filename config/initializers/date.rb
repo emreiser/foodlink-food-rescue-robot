ff337a314ed3e5d32d95aac3f2efff2e17c4831a
emreiser <emilie.reiser@gmail.com>
2017-09-10 22:52:49 -0400

    Count weeks based on weekday not calendar line

--- config/initializers/date.rb
+++ config/initializers/date.rb
@@ -1,11 +1,6 @@
 class Date
-  def week_of_year(mondays = false)
-    # Use %U for weeks starting on Sunday
-    # Use %W for weeks starting on Monday
-    strftime(mondays ? "%W" : "%U").to_i + 1
-  end
-
-  def week_of_month(mondays = false)
-    week_of_year(mondays) - beginning_of_month.week_of_year(mondays) + 1
+  def week_of_month
+    # week_of_year(mondays) - beginning_of_month.week_of_year(mondays) + 1
+    ((self.day - 1) / 7).floor + 1
   end
 end
